d49e86cac9d8939ea9a0b7f7c7d6e702932106d9
MDaineko <42941961+MDaineko@users.noreply.github.com>
2020-12-13 22:20:03 +0300

    init: start Lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1
+{
+    class A
+    {
+        private int a = 3;
+        private int b = 5;
+        public int c
+        {
+            get
+            {
+                return a *= (a + b);
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A classA = new A();
+            Console.WriteLine(classA.c);
+            Console.ReadKey();
+        }
+    }
+}
