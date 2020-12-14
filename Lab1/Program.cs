c543b7a09749edd9b55de436743123b956a51562
MDaineko <42941961+MDaineko@users.noreply.github.com>
2020-12-15 01:57:57 +0300

    init: start Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -2,26 +2,24 @@
 
 namespace Lab1
 {
-    class A
-    {
-        private int a = 3;
-        private int b = 5;
-        public int c
-        {
-            get
-            {
-                return a *= (a + b);
-            }
-        }
-    }
-    
     class Program
     {
         static void Main(string[] args)
         {
             A classA = new A();
-            Console.WriteLine(classA.c);
+            Console.WriteLine("Значение свойства с: " + classA.c);
             Console.ReadKey();
+        }
+    }
+    
+    class A
+    {
+        private int a = 3;
+        private int b = 5;
+        
+        public int c
+        {
+            get { return a *= (a + b); }
         }
     }
 }
